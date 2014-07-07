5ca29c367088cb2db23ae524395f57652fee91b1
markusz <markus.ziller@netlight.com>
2014-07-07 10:12:40 +0200

    Fixes

--- src/model/pieces/Rook.java
+++ src/model/pieces/Rook.java
@@ -5,17 +5,56 @@ import model.Player;
 import model.Square;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Set;
 
 public class Rook extends AbstractStraightMovingPiece {
 
-  public Rook(Player player){
-    super(player);
-    textualRepresentation = "R";
-  }
+	public Rook(Player player) {
+		super(player);
+		textualRepresentation = "R";
+	}
 
-  @Override
-  public void updatePossibleMoves() {
+	@Override
+	public void updatePossibleMoves() {
+		boolean isProtectingHisKing = isProtectingHisKing();
+		if (isProtectingHisKing) {
+			normalMoves.clear();
+		}
 
-  }
+		List<Square> up = getSquaresInStraightLineUntilFirstPiece(AbstractStraightMovingPiece.UP, AbstractStraightMovingPiece.STAY);
+		List<Square> down = getSquaresInStraightLineUntilFirstPiece(AbstractStraightMovingPiece.DOWN, AbstractStraightMovingPiece.STAY);
+		List<Square> right = getSquaresInStraightLineUntilFirstPiece(AbstractStraightMovingPiece.STAY, AbstractStraightMovingPiece.RIGHT);
+		List<Square> left = getSquaresInStraightLineUntilFirstPiece(AbstractStraightMovingPiece.STAY, AbstractStraightMovingPiece.LEFT);
+
+		List<Square> all = new LinkedList<>();
+		all.addAll(up);
+		all.addAll(down);
+		all.addAll(left);
+		all.addAll(right);
+
+		for (Square square : all) {
+			if(checks(square.getPiece())){
+				//TODO piece is check
+			}
+			if (square.isEmpty() && !isProtectingHisKing) {
+				//TODO set to moves
+				//TODO add to threatened squares
+			}
+			if (!square.isEmpty() && !square.getPiece().isSameColor(this)) {
+				if (square.getPiece() instanceof King) {
+					//TODO opponent is check
+				} else {
+					if (isProtectingHisKing) {
+						//TODO piece is threatened
+					} else {
+						//TODO piece can be thrown
+					}
+				}
+			}
+		}
+	}
+
+
 }
