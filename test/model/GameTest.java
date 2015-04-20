e4884770c8e92546be064c719c52fb6b48c80dad
markusz <markus.ziller@netlight.com>
2015-04-20 11:00:14 +0200

    Added GameTest.java

---
+++ test/model/GameTest.java
@@ -0,0 +1,30 @@
+package model;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
+	@Test
+	public void testGetWhitePlayer() throws Exception {
+		assertFalse(false);
+	}
+
+	@Test
+	public void testGetBlackPlayer() throws Exception {
+
+	}
+}
